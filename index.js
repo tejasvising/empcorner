55f31422ebfcefcc07419adaba0d3f712ad28462
tejasvising <85549686+tejasvising@users.noreply.github.com>
2023-08-05 21:57:49 +0530

    updated both files

--- index.js
+++ index.js
@@ -326,7 +326,7 @@ app.get('/', (req, res) => {
 passport.use(new GoogleStrategy({
     clientID:     GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback",
+    callbackURL: "/auth/google/callback",
     passReqToCallback   : true
   },
   function(request, accessToken, refreshToken, profile, done) {
